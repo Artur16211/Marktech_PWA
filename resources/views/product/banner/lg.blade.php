68780323830e9fc99c8a33f978eb0361fc8ab74e
Artur16211 <herrera.alvaradoartu@gmail.com>
2022-10-09 13:52:08 -0500

    mobile UI test - pagination

--- resources/views/product/banner/lg.blade.php
+++ resources/views/product/banner/lg.blade.php
@@ -6,7 +6,14 @@
             <input type="text" class="form-control mx-auto" name="barra" placeholder="Buscar con filtros..."
                 style="width: 300px; height: 50px"><br>
 
-                {!! $viewData['banner_lg']->withQueryString()->links('layouts.pagination') !!}
+
+                <div class="hide-mobile">
+                    {!! $viewData['banner_lg']->withQueryString()->links('layouts.pagination') !!}
+                </div>
+
+                <div class="hide-desktop mobile-pagination">
+                    {!! $viewData['banner_lg']->withQueryString()->links('layouts.paginationm') !!}
+                </div>
 
             <div class="position-absolute top-0 start-0">
                 {{-- sort select --}}
@@ -222,7 +229,13 @@
 @endforeach
 @endif
 
-{!! $viewData['banner_lg']->withQueryString()->links('layouts.pagination') !!}
+<div class="hide-mobile">
+    {!! $viewData['banner_lg']->withQueryString()->links('layouts.pagination') !!}
+</div>
+
+<div class="hide-desktop mobile-pagination">
+    {!! $viewData['banner_lg']->withQueryString()->links('layouts.paginationm') !!}
+</div>
 
 
 
