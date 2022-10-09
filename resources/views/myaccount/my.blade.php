32e805f64898f38eb849cf33014dbf152aa2c98a
Artur16211 <herrera.alvaradoartu@gmail.com>
2022-10-08 19:25:36 -0500

    mobile UI test - logout

--- resources/views/myaccount/my.blade.php
+++ resources/views/myaccount/my.blade.php
@@ -1,34 +1,40 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="container">
-    <div class="col">
-        <div class="p-3 border-top border-bottom bg-light">
-            <img src="{{ asset('img/user.png') }}" alt="Logo" class="rounded mx-auto d-block" />
-            <h1 class="h3 text-center">{{ $viewData['user'] }}</h1>
-       </div>
-</div>
-<br>
-<div class="container">
-    <div class="col">
-        <div class="p-3 border-top border-bottom bg-light"><strong>Mis Datos</strong></div>
-        <div class="p-3 border-top border-bottom bg-light">Modifica tus datos de usuario. <a class="btn btn-link float-end" href="/miusuario" role="button">></a></div>
-       </div>
-</div>
-<br>
-<div class="container">
-    <div class="col">
-        <div class="p-3 border-top border-bottom bg-light"><strong>Direcciones</strong></div>
-        <div class="p-3 border-top border-bottom bg-light">Modifica o agrega direcciones. <a class="btn btn-link float-end" href="/misdatos" role="button">></a></div>
-       </div>
-</div>
-<br>
-<div class="container">
-    <div class="col">
-        <div class="p-3 border-top border-bottom bg-light"><strong>Compras</strong></div>
-        <div class="p-3 border-top border-bottom bg-light">Ver todas mis compras. <a class="btn btn-link float-end" href="/pedidos" role="button">></a></div>
-       </div>
-</div>
-<br>
-
-@endsection
+    <div class="container">
+        <div class="col">
+            <div class="p-3 border-top border-bottom bg-light">
+                <img src="{{ asset('img/user.png') }}" alt="Logo" class="rounded mx-auto d-block" />
+                <h1 class="h3 text-center">{{ $viewData['user'] }}</h1>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="col">
+                <div class="p-3 border-top border-bottom bg-light"><strong>Mis Datos</strong></div>
+                <div class="p-3 border-top border-bottom bg-light">Modifica tus datos de usuario. <a
+                        class="btn btn-link float-end" href="/miusuario" role="button">></a></div>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="col">
+                <div class="p-3 border-top border-bottom bg-light"><strong>Direcciones</strong></div>
+                <div class="p-3 border-top border-bottom bg-light">Modifica o agrega direcciones. <a
+                        class="btn btn-link float-end" href="/misdatos" role="button">></a></div>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="col">
+                <div class="p-3 border-top border-bottom bg-light"><strong>Compras</strong></div>
+                <div class="p-3 border-top border-bottom bg-light">Ver todas mis compras. <a class="btn btn-link float-end"
+                        href="/pedidos" role="button">></a></div>
+            </div>
+        </div>
+        <br>
+        <form id="logout" action="{{ route('logout') }}" method="POST">
+            <a role="button" class="btn btn-primary mx-auto d-block" onclick="document.getElementById('logout').submit();">Cerrar Sesión</a>
+            @csrf
+        </form>
+        <br>
+    @endsection
