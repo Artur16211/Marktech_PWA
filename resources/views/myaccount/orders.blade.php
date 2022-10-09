334b1f72fe7bde57c210ef80712af0342971f69e
Artur16211 <herrera.alvaradoartu@gmail.com>
2022-10-09 18:54:13 -0500

    mobile UI test - color fix

--- resources/views/myaccount/orders.blade.php
+++ resources/views/myaccount/orders.blade.php
@@ -106,7 +106,7 @@
                                             alt="{{ $item->getProduct()->getName() }}" class="img-fluid" width="100">
                                     </td>
                                     <td>
-                                        <a class="link-primary"
+                                        <a class="link-info"
                                             href="{{ route('product.show', ['id' => $item->getProduct()->getId()]) }}">
                                             {{ $item->getProduct()->getName() }}
                                         </a>
