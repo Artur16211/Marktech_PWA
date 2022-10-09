7c3c36fc2b7f436cad161c585c27d95169da4282
Artur16211 <herrera.alvaradoartu@gmail.com>
2022-10-09 00:49:43 -0500

    mobile UI test - settings

--- resources/views/mobile/settings/theme.blade.php
+++ resources/views/mobile/settings/theme.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 @section('content')
+
+    <div class="form-check form-switch">
+        <input type="checkbox" class="form-check-input" id="darkSwitch" />
+        <label class="custom-control-label" for="darkSwitch">Modo Oscuro</label>
+    </div>
+
 @endsection
--- resources/views/mobile/settingsm.blade.php
+++ resources/views/mobile/settingsm.blade.php
@@ -16,6 +16,15 @@
             <p class="mb-1">Temas</p>
             <small class="text-muted">Cambiar la apariencia de la aplicación.</small>
         </a>
+        <a href="/languagem" class="list-group-item list-group-item-action" aria-current="true">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1"><span class="iconify" data-icon="clarity:language-line" style="font-size: 32px;"></span></h5>
+                <small class="text-muted"><span class="iconify" data-icon="bi:arrow-right-circle"
+                        style="font-size: 32px;"></span></small>
+            </div>
+            <p class="mb-1">Idioma</p>
+            <small class="text-muted"></small>
+        </a>
         <a href="/aboutm" class="list-group-item list-group-item-action" aria-current="true">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1"><span class="iconify" data-icon="fluent:info-20-regular" style="font-size: 32px;"></span>
