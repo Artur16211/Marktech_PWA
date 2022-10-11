79b80a1f850a8ddf248d5ba1fe58e3448bce5c52
Artur16211 <herrera.alvaradoartu@gmail.com>
2022-10-11 01:35:21 -0500

    mobile UI test - offline test

--- public/offline/settings/theme.php
+++ public/offline/settings/theme.php
@@ -369,19 +369,7 @@
         </blockquote>
     </figure>
     <div class="form-check form-switch">
-        <input type="checkbox" class="form-check-input" id="darkSwitch" />
+        <input type="checkbox" class="form-check-input" id="darkSwitch"/>
 
         <label class="custom-control-label" for="darkSwitch">Modo Oscuro (BETA)</label>
     </div>
-
-
-
-    {{-- <input type="checkbox" id="toggle_checkbox">
-
-    <label for="toggle_checkbox">
-        <div id="star">
-            <div class="star" id="star-1">★</div>
-            <div class="star" id="star-2">★</div>
-        </div>
-        <div id="moon"></div>
-    </label> --}}
